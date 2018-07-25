7a87912437f8068e5d00f726d65ae55958d249c8
tony.chow <tony.chow@hmcts.net>
2018-07-25 13:47:17 +0100

    Disable address rules for D8

--- src/main/java/uk/gov/hmcts/reform/divorce/validationservice/rules/divorce/d8/D8PetitionerHomeAddress.java
+++ src/main/java/uk/gov/hmcts/reform/divorce/validationservice/rules/divorce/d8/D8PetitionerHomeAddress.java
@@ -1,41 +1,41 @@
-package uk.gov.hmcts.reform.divorce.validationservice.rules.divorce.d8;
+// package uk.gov.hmcts.reform.divorce.validationservice.rules.divorce.d8;
 
-import com.deliveredtechnologies.rulebook.annotation.Given;
-import com.deliveredtechnologies.rulebook.annotation.Result;
-import com.deliveredtechnologies.rulebook.annotation.Rule;
-import com.deliveredtechnologies.rulebook.annotation.Then;
-import com.deliveredtechnologies.rulebook.annotation.When;
-import lombok.Data;
-import uk.gov.hmcts.reform.divorce.validationservice.domain.request.CoreCaseData;
+// import com.deliveredtechnologies.rulebook.annotation.Given;
+// import com.deliveredtechnologies.rulebook.annotation.Result;
+// import com.deliveredtechnologies.rulebook.annotation.Rule;
+// import com.deliveredtechnologies.rulebook.annotation.Then;
+// import com.deliveredtechnologies.rulebook.annotation.When;
+// import lombok.Data;
+// import uk.gov.hmcts.reform.divorce.validationservice.domain.request.CoreCaseData;
 
-import java.util.List;
-import java.util.Optional;
+// import java.util.List;
+// import java.util.Optional;
 
-@Rule(order = 11)
-@Data
-public class D8PetitionerHomeAddress {
+// @Rule(order = 11)
+// @Data
+// public class D8PetitionerHomeAddress {
 
-    private static final String BLANK_SPACE = " ";
-    private static final String ACTUAL_DATA = "Actual data is: %s";
-    private static final String ERROR_MESSAGE = "D8PetitionerHomeAddress can not be null or empty.";
+//     private static final String BLANK_SPACE = " ";
+//     private static final String ACTUAL_DATA = "Actual data is: %s";
+//     private static final String ERROR_MESSAGE = "D8PetitionerHomeAddress can not be null or empty.";
 
-    @Result
-    public List<String> result;
+//     @Result
+//     public List<String> result;
 
-    @Given("coreCaseData")
-    public CoreCaseData coreCaseData = new CoreCaseData();
+//     @Given("coreCaseData")
+//     public CoreCaseData coreCaseData = new CoreCaseData();
 
-    @When
-    public boolean when() {
-        return !Optional.ofNullable(coreCaseData.getD8PetitionerHomeAddress()).isPresent();
-    }
+//     @When
+//     public boolean when() {
+//         return !Optional.ofNullable(coreCaseData.getD8PetitionerHomeAddress()).isPresent();
+//     }
 
-    @Then
-    public void then() {
-        result.add(String.join(
-            BLANK_SPACE, // delimiter
-            ERROR_MESSAGE,
-            String.format(ACTUAL_DATA, coreCaseData.getD8PetitionerHomeAddress())
-        ));
-    }
-}
+//     @Then
+//     public void then() {
+//         result.add(String.join(
+//             BLANK_SPACE, // delimiter
+//             ERROR_MESSAGE,
+//             String.format(ACTUAL_DATA, coreCaseData.getD8PetitionerHomeAddress())
+//         ));
+//     }
+// }
--- src/test/java/uk/gov/hmcts/reform/divorce/validationservice/rules/divorce/d8/D8PetitionerCorrespondenceAddressTest.java
+++ src/test/java/uk/gov/hmcts/reform/divorce/validationservice/rules/divorce/d8/D8PetitionerCorrespondenceAddressTest.java
@@ -1,57 +1,57 @@
-package uk.gov.hmcts.reform.divorce.validationservice.rules.divorce.d8;
+// package uk.gov.hmcts.reform.divorce.validationservice.rules.divorce.d8;
 
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import uk.gov.hmcts.reform.divorce.validationservice.domain.request.Address;
-import uk.gov.hmcts.reform.divorce.validationservice.domain.request.CoreCaseData;
+// import org.junit.Before;
+// import org.junit.Test;
+// import org.junit.runner.RunWith;
+// import org.springframework.boot.test.context.SpringBootTest;
+// import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+// import uk.gov.hmcts.reform.divorce.validationservice.domain.request.Address;
+// import uk.gov.hmcts.reform.divorce.validationservice.domain.request.CoreCaseData;
 
-import java.util.ArrayList;
+// import java.util.ArrayList;
 
-import static org.junit.Assert.assertEquals;
+// import static org.junit.Assert.assertEquals;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringBootTest
-public class D8PetitionerCorrespondenceAddressTest {
+// @RunWith(SpringJUnit4ClassRunner.class)
+// @SpringBootTest
+// public class D8PetitionerCorrespondenceAddressTest {
 
-    private D8PetitionerCorrespondenceAddress rule;
-    private CoreCaseData coreCaseData;
+//     private D8PetitionerCorrespondenceAddress rule;
+//     private CoreCaseData coreCaseData;
 
-    @Before
-    public void setup() {
-        rule = new D8PetitionerCorrespondenceAddress();
-        coreCaseData = new CoreCaseData();
-    }
+//     @Before
+//     public void setup() {
+//         rule = new D8PetitionerCorrespondenceAddress();
+//         coreCaseData = new CoreCaseData();
+//     }
 
-    @Test
-    public void whenShouldReturnTrueWhenD8PetitionerCorrespondenceAddressIsNull() {
-        rule.setCoreCaseData(coreCaseData);
-        boolean result = rule.when();
+//     @Test
+//     public void whenShouldReturnTrueWhenD8PetitionerCorrespondenceAddressIsNull() {
+//         rule.setCoreCaseData(coreCaseData);
+//         boolean result = rule.when();
         
-        assertEquals(true, result);
-    }
+//         assertEquals(true, result);
+//     }
 
-    @Test
-    public void whenShouldReturnFalseWhenD8PetitionerCorrespondenceAddressIsNotNull() {
-        Address address = new Address();
-        coreCaseData.setD8PetitionerCorrespondenceAddress(address);
+//     @Test
+//     public void whenShouldReturnFalseWhenD8PetitionerCorrespondenceAddressIsNotNull() {
+//         Address address = new Address();
+//         coreCaseData.setD8PetitionerCorrespondenceAddress(address);
 
-        rule.setCoreCaseData(coreCaseData);
-        boolean result = rule.when();
+//         rule.setCoreCaseData(coreCaseData);
+//         boolean result = rule.when();
 
-        assertEquals(false, result);
-    }
+//         assertEquals(false, result);
+//     }
 
-    @Test
-    public void thenShouldReturnErrorMessageWithNull() {
-        rule.setCoreCaseData(coreCaseData);
+//     @Test
+//     public void thenShouldReturnErrorMessageWithNull() {
+//         rule.setCoreCaseData(coreCaseData);
 
-        rule.setResult(new ArrayList<>());
-        rule.then();
+//         rule.setResult(new ArrayList<>());
+//         rule.then();
 
-        assertEquals("D8PetitionerCorrespondenceAddress can not be null or empty. Actual data is: null",
-            rule.getResult().get(0));
-    }
-}
+//         assertEquals("D8PetitionerCorrespondenceAddress can not be null or empty. Actual data is: null",
+//             rule.getResult().get(0));
+//     }
+// }
