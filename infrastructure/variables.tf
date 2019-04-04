14ee56d5a74a5fba01e2cfb7db33e9452937244e
Tony Chow <tony.chow@hmcts.net>
2019-04-04 15:41:44 +0100

    Use Shared App InsightsC

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -28,3 +28,8 @@ variable "jenkins_AAD_objectId" {
 variable "common_tags" {
   type = "map"
 }
+
+variable "appinsights_instrumentation_key" {
+  description = "Instrumentation key of the App Insights instance this webapp should use. Module will create own App Insights resource if this is not provided"
+  default = ""
+}
