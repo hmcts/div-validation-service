06153dbfb06f4cf035cbc6ef9458f3a549967d62
tony.chow <tony.chow@hmcts.net>
2019-06-07 15:04:46 +0100

    Adjust instance size by var

---
+++ infrastructure/aat.tfvars
@@ -0,0 +1,1 @@
+instance_size = "I3"
--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -33,3 +33,7 @@ variable "appinsights_instrumentation_key" {
   description = "Instrumentation key of the App Insights instance this webapp should use. Module will create own App Insights resource if this is not provided"
   default = ""
 }
+
+variable "instance_size" {
+  default = "I2"
+}
