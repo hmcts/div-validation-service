cf4c494c5d05dbd0264093fc3e7a2f678db1b292
Tony Chow <36265570+tchow8@users.noreply.github.com>
2019-06-07 13:13:51 +0100

    Set instance size to I3 (#64)

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -17,6 +17,7 @@ module "div-validation-service" {
   common_tags                     = "${var.common_tags}"
   asp_name                        = "${local.asp_name}"
   asp_rg                          = "${local.asp_rg}"
+  instance_size                   = "I3"
 
   app_settings = {
     //    logging vars
